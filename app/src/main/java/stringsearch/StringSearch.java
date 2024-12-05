18e7a9035de7cba8fe310500a4b61c632f6f2d33
Angelica Catherine Larsson <an7061la-s@student.lu.se>
2024-12-05 11:36:08 +0100

    Added suggested changes

--- app/src/main/java/stringsearch/StringSearch.java
+++ app/src/main/java/stringsearch/StringSearch.java
@@ -7,17 +7,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StringSearch {
+    static String pattern;
+    static String fileName;
 
     public static void main(String[] args) {
-        // Check if arguments are provided
-        if (args.length < 2) {
-            System.out.println("Usage: search <pattern> <file>");
-            return;
-        }
-
-        // Extract the search pattern and file name from arguments
-        String pattern = args[0];
-        String fileName = args[1];
+        checkInput(args);
 
         try {
             // Read the file and search for the pattern
@@ -38,10 +32,40 @@ public class StringSearch {
     }
 
     /**
+     * Validates the input arguments and assigns values to the pattern and file
+     * name.
+     * <p>
+     * This method checks if the required arguments (search pattern and file name)
+     * are provided. If not, it prints a usage message and exits the program. If the
+     * arguments are valid, the pattern and file name are assigned to the
+     * corresponding
+     * static variables for further processing.
+     * </p>
+     *
+     * @param args An array of command-line arguments:
+     *             - args[0]: The search pattern to look for.
+     *             - args[1]: The file name to search within.
+     * 
+     *             Note: This method exits early without performing further
+     *             operations if the
+     *             arguments are insufficient.
+     */
+    private static void checkInput(String[] args) {
+        // Check if arguments are provided and have correct usage.
+        if (args.length < 2) {
+            System.out.println("Usage: search <pattern> <file>");
+            return;
+        }
+
+        pattern = args[0];
+        fileName = args[1];
+    }
+
+    /**
      * Searches for the given pattern in the specified file and returns matching
      * lines.
      *
-     * @param pattern The string to search for.
+     * @param pattern  The string to search for.
      * @param fileName The name of the file to search in.
      * @return A list of lines containing the pattern.
      * @throws IOException If an I/O error occurs.
